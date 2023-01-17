f57de375fa6e354e123941cd763dadda0c9501db
AlexanderTumanov <alexander.tumanov.jr@gmail.com>
2023-01-16 20:49:37 -0500

    pathfinding for ghosts implementation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var PModel = {
     ["1HHHHHHHHHHHH21HHHHHHHHHHHH2"],
     ["VooooooooooooVVooooooooooooV"],
     ["Vo1HH2o1HHH2oVVo1HHH2o1HH2oV"],
-    ["VOV  VoV   VoVVoV   VoV  VOV"],
+    ["VoV  VoV   VoVVoV   VoV  VoV"],
     ["Vo3HH4o3HHH4o34o3HHH4o3HH4oV"],
     ["VooooooooooooooooooooooooooV"],
     ["Vo1HH2o12o1HHHHHH2o12o1HH2oV"],
@@ -11,10 +11,10 @@ var PModel = {
     ["VooooooVVooooVVooooVVooooooV"],
     ["3HHHH2oV3HH2 VV 1HH4Vo1HHHH4"],
     ["     VoV1HH4 34 3HH2VoV     "],
-    ["     VoVV          VVoV     "],
+    ["     VoVV    5     VVoV     "],
     ["     VoVV 1HHZZHH2 VVoV     "],
     ["3HHHH4o34 V      V 34o3HHHH4"],
-    ["      o   V      V   o      "],
+    ["      o   V6  7 8V   o      "],
     ["1HHHH2o12 V      V 12o1HHHH2"],
     ["     VoVV 3HHHHHH4 VVoV     "],
     ["     VoVV          VVoV     "],
@@ -32,11 +32,124 @@ var PModel = {
     ["VooooooooooooooooooooooooooV"],
     ["3HHHHHHHHHHHHHHHHHHHHHHHHHH4"],
   ],
+  ghosts: [],
 };
 
-var Pacman = {
+function Ghost() {
+  this.id;
+  this.angle = "90";
+  this.pastEl = " ";
+}
+Ghost.prototype = {
+  angRandom: function () {
+    const angles = ["0", "90", "180", "270"];
 
+    const randomAng = Math.floor(Math.random() * 4);
+    this.angle = angles[randomAng];
+  },
+
+  moveGhost: function () {
+    switch (this.angle) {
+      case "270":
+        if (PController.checkifEmpty(this.y - 1, this.x, this.angle)) {
+          this.moveG(this.y - 1, this.x, this.angle);
+        } else {
+          let arr = [
+            { a: "0", d: this.getDistance(this.y, this.x + 1) },
+            { a: "90", d: this.getDistance(this.y + 1, this.x) },
+            { a: "180", d: this.getDistance(this.y, this.x - 1) },
+          ];
+          this.moveGhost1(arr);
+        }
+        //this.moveG(this.y-1,this.x, this.angle);
+        break;
+      case "0":
+        if (PController.checkifEmpty(this.y, this.x + 1, this.angle)) {
+          this.moveG(this.y, this.x + 1, this.angle);
+        } else {
+          let arr = [
+            { a: "270", d: this.getDistance(this.y - 1, this.x) },
+            { a: "90", d: this.getDistance(this.y + 1, this.x) },
+            { a: "180", d: this.getDistance(this.y, this.x - 1) },
+          ];
+          this.moveGhost1(arr);
+        }
+
+        break;
+      case "90":
+        if (PController.checkifEmpty(this.y + 1, this.x, this.angle)) {
+          this.moveG(this.y + 1, this.x, this.angle);
+        } else {
+          let arr = [
+            { a: "0", d: this.getDistance(this.y, this.x + 1) },
+            { a: "270", d: this.getDistance(this.y - 1, this.x) },
+            { a: "180", d: this.getDistance(this.y, this.x - 1) },
+          ];
+          this.moveGhost1(arr);
+        }
+        break;
+      case "180":
+        if (PController.checkifEmpty(this.y, this.x - 1, this.angle)) {
+          this.moveG(this.y, this.x - 1, this.angle);
+        } else {
+          let arr = [
+            { a: "270", d: this.getDistance(this.y - 1, this.x) },
+            { a: "90", d: this.getDistance(this.y + 1, this.x) },
+            { a: "0", d: this.getDistance(this.y, this.x + 1) },
+          ];
+          this.moveGhost1(arr);
+        }
+        break;
+    }
+    PController.view.draw();
+  },
+
+  getDistance: function (r, c) {
+    if (!PController.checkifEmpty(r, c, this.angle)) {
+      return 100000;
+    }
+    return Math.pow(Pacman.y - r, 2) + Math.pow(Pacman.x - c, 2);
+  },
+
+  moveGhost1: function (arr) {
+    arr.sort(function (a, b) {
+      return a.d - b.d;
+    });
+    this.angle = arr[0].a;
+    this.moveGhost();
+  },
+
+  moveG: function (r, c, angle) {
+    if (PController.checkifEmpty(r, c)) {
+      this.model.mazeArray[this.y][this.x] = this.pastEl;
+      this.y = r;
+      this.x = c;
+      this.pastEl = this.model.mazeArray[this.y][this.x];
+      this.model.mazeArray[this.y][this.x] = this.id;
+      this.angle = angle;
+      PController.view.draw();
+      return true;
+    } else {
+      switch (this.angle) {
+        case "270":
+          if (Pacman.x > this.x) {
+          } else if (Pacman.x > this.x) {
+          }
+          break;
+        case "0":
+          break;
+        case "90":
+          break;
+        case "180":
+          break;
+      }
+      this.angRandom();
+      this.moveGhost();
+    }
+  },
 };
+
+var Pacman = {};
 
 var PView = {
   model: null,
@@ -46,6 +159,14 @@ var PView = {
   init: function (model, cellSize, mainDiv) {
     this.model = model;
     this.mainDiv = mainDiv;
+    for (let i = 0; i < 4; i++) {
+      model.ghosts[i] = new Ghost();
+      model.ghosts[i].model = model;
+    }
+    model.ghosts[0].id = 5;
+    model.ghosts[1].id = 6;
+    model.ghosts[2].id = 7;
+    model.ghosts[3].id = 8;
     var s = "",
       row;
     this.cells = [];
@@ -58,10 +179,15 @@ var PView = {
       }
       for (let c = 0, cols = model.mazeArray[r].length; c < cols; c++) {
         row.push(null);
-        if (model.mazeArray[r][c] === "P") {
+        let x = model.mazeArray[r][c];
+        if (x === "P") {
           Pacman.x = c;
           Pacman.y = r;
-          Pacman.angle = '0';
+          Pacman.angle = "0";
+        } else if (x >= "5" && x <= "8") {
+          // let f=parseInt(x);
+          model.ghosts[x - 5].x = c;
+          model.ghosts[x - 5].y = r;
         }
         s +=
           '<DIV id="' +
@@ -98,7 +224,7 @@ var PView = {
     for (let r = 0, rows = this.model.mazeArray.length; r < rows; r++) {
       for (let c = 0, cols = this.model.mazeArray[r].length; c < cols; c++) {
         this.cells[r][c].setAttribute("type", this.model.mazeArray[r][c]);
-        if(this.model.mazeArray[r][c]==='P'){
+        if (this.model.mazeArray[r][c] === "P") {
           this.cells[r][c].setAttribute("angle", Pacman.angle);
         }
       }
@@ -121,8 +247,16 @@ var PController = {
   },
   oneTik: function () {
     PController.moveNext();
+    for (let i = 0; i < 4; i++) {
+      this.model.ghosts[i].moveGhost();
+    }
     //next tik
-    PController.wait(PController.tik, PController.oneTik, PController, PController.view);
+    PController.wait(
+      PController.tik,
+      PController.oneTik,
+      PController,
+      PController.view
+    );
   },
   wait: function (milliseconds, foo, ctx, view) {
     setTimeout(function () {
@@ -146,69 +280,68 @@ var PController = {
         this.moveUp();
         break;
       //case 32: //space
-        //this.drop();
-        //break;
+      //this.drop();
+      //break;
       //case 27: //escape
-        //this.pause();
-        //break;
+      //this.pause();
+      //break;
       //case 90:
       //case 122: //z or Z
-        //this.rotateLeft();
-        //break;
+      //this.rotateLeft();
+      //break;
     }
   },
 
-  moveNext : function () {
-    switch(Pacman.angle){
-      case '270':
-        this.moveP(Pacman.y-1,Pacman.x, Pacman.angle);
-        break; 
-      case '0':
-        this.moveP(Pacman.y,Pacman.x+1,Pacman.angle);
-        break; 
-      case '90':
-        this.moveP(Pacman.y+1,Pacman.x, Pacman.angle);
-        break; 
-      case '180':
-        this.moveP(Pacman.y,Pacman.x-1, Pacman.angle);
+  moveNext: function () {
+    switch (Pacman.angle) {
+      case "270":
+        this.moveP(Pacman.y - 1, Pacman.x, Pacman.angle);
+        break;
+      case "0":
+        this.moveP(Pacman.y, Pacman.x + 1, Pacman.angle);
+        break;
+      case "90":
+        this.moveP(Pacman.y + 1, Pacman.x, Pacman.angle);
+        break;
+      case "180":
+        this.moveP(Pacman.y, Pacman.x - 1, Pacman.angle);
         break;
     }
     this.view.draw();
   },
-  moveP : function (r, c, angle) {
-    if(this.checkifEmpty(r,c)){
-      this.model.mazeArray[Pacman.y][Pacman.x]=' ';
+  moveP: function (r, c, angle) {
+    if (this.checkifEmpty(r, c)) {
+      this.model.mazeArray[Pacman.y][Pacman.x] = " ";
       Pacman.y = r;
       Pacman.x = c;
-      this.model.mazeArray[Pacman.y][Pacman.x]='P';
+      this.model.mazeArray[Pacman.y][Pacman.x] = "P";
       Pacman.angle = angle;
       this.view.draw();
       return true;
-
     }
   },
 
   moveDown: function () {
-    if (this.checkifFacing('90')){
-      this.moveP(Pacman.y+1,Pacman.x, '90');
+    if (this.checkifFacing("90")) {
+      this.moveP(Pacman.y + 1, Pacman.x, "90");
     }
   },
   moveLeft: function () {
-    if (this.checkifFacing('180')){
-      this.moveP(Pacman.y,Pacman.x-1, '180');
+    if (this.checkifFacing("180")) {
+      this.moveP(Pacman.y, Pacman.x - 1, "180");
     }
   },
   moveRight: function () {
-    if (this.checkifFacing('0')){
-      this.moveP(Pacman.y,Pacman.x+1, '0');
+    if (this.checkifFacing("0")) {
+      this.moveP(Pacman.y, Pacman.x + 1, "0");
     }
   },
   moveUp: function () {
-    if (this.checkifFacing('270')){
-      this.moveP(Pacman.y-1,Pacman.x, '270');
+    if (this.checkifFacing("270")) {
+      this.moveP(Pacman.y - 1, Pacman.x, "270");
     }
   },
-  
+
   drop: function () {
     this.f.drop();
     this.model.removeFullLines();
@@ -219,18 +352,20 @@ var PController = {
   pause: function () {},
 
   checkifFacing: function (angle) {
-    if (Pacman.angle != angle){
+    if (Pacman.angle != angle) {
       return true;
     }
     return false;
   },
 
-  checkifEmpty: function (r,c) {
-    if(r<0||r>=this.model.mazeArray.length)return false;
-    if(c<0||c>=this.model.mazeArray[c].length)return false;
-    switch(this.model.mazeArray[r][c]){
-      case 'o':case ' ':  return true;
+  checkifEmpty: function (r, c) {
+    if (r < 0 || r >= this.model.mazeArray.length) return false;
+    if (c < 0 || c >= this.model.mazeArray[c].length) return false;
+    switch (this.model.mazeArray[r][c]) {
+      case "o":
+      case " ":
+        return true;
     }
     return false;
-  }
+  },
 };
